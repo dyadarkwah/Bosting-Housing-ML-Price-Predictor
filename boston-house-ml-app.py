d1cc555fa9b89bf3160ca265d8dee3e38f486bce
David Adarkwah <81932818+dyadarkwah@users.noreply.github.com>
2021-06-27 18:30:38 -0400

    Add files via upload

--- boston-house-ml-app.py
+++ boston-house-ml-app.py
@@ -76,7 +76,7 @@ st.write('---')
 explainer = shap.TreeExplainer(model)
 shap_values = explainer.shap_values(X)
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+# st.set_option('deprecation.showPyplotGlobalUse', False)
 st.header('Feature Importance')
 plt.title('Feature importance based on SHAP values')
 shap.summary_plot(shap_values, X)
